a7097deae7e5b14921f0ced8e0f34d4cbd9932cc
Xucong Zhan <zhanxucong@gmail.com>
2018-09-28 01:53:09 -0400

    Working on Chapter 3: Web Forms

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, flash, redirect
+
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Xucong'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'This is a beautiful day!'
+        },
+        {
+            'author': {'username': 'Sally'},
+            'body': 'The Matrix is a great movie!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me = {}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+
+    return render_template('login.html', title="User Login", form=form)
