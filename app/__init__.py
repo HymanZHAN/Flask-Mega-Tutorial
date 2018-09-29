d89bd1e9002d173ab9417e5fd1f5e387d5ed45aa
Xucong Zhan <zhanxucong@gmail.com>
2018-09-28 23:02:56 -0400

    Working on Chapter 4: Database Relationships

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # for preventing circular dependencies
+from app import routes, models  # for preventing circular dependencies
