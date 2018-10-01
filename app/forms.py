7b705f976a47d4dce38dc05c9193b569c07f2e64
Xucong Zhan <zhanxucong@gmail.com>
2018-09-30 23:47:37 -0400

    Finished Chapter 5: User Registration

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
+from app.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms. validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,25 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegitrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_re = PasswordField(
+        'Repeat Password',
+        validators=[DataRequired(),
+                    EqualTo('password',
+                            message='Two passwords do not match!')])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Please use a different user name.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Please use a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,68 @@
-from flask import render_template, flash, redirect, url_for
-
-from app import app
-from app.forms import LoginForm
+from app import  app, db
+from app.models import User
+from app.forms import LoginForm, RegitrationForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Xucong'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'This is a beautiful day!'
+    posts = [{
+        'author': {
+            'username': 'John'
         },
-        {
-            'author': {'username': 'Sally'},
-            'body': 'The Matrix is a great movie!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+        'body': 'This is a beautiful day!'
+    }, {
+        'author': {
+            'username': 'Sally'
+        },
+        'body': 'The Matrix is a great movie!'
+    }]
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me = {}'.format(
-            form.username.data, form.remember_me.data
-        ))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
 
     return render_template('login.html', title="User Login", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegitrationForm()
+
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful! Welcome to Microblog!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Regester', form=form)
